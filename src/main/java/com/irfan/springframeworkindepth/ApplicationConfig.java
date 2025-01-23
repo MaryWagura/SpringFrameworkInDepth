733dc394a5f0a0258f9146d0db717a9ebea8f9ed
Mary Wagura <mwagura@ipsl.co.ke>
2025-01-23 14:58:29 +0300

    Application Config

--- src/main/java/com/irfan/springframeworkindepth/ApplicationConfig.java
+++ src/main/java/com/irfan/springframeworkindepth/ApplicationConfig.java
@@ -1,9 +1,16 @@
 package com.irfan.springframeworkindepth;
 
+import com.irfan.springframeworkindepth.service.TimeService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
 
 public class ApplicationConfig {
     @Value("hello")
     private String greetings;
+
+    @Bean
+    public TimeService timeService() {
+        return new TimeService();
+    }
 }
