126b4aa741ae130bcbc880777d69822957d04a8d
Mary Wagura <mwagura@ipsl.co.ke>
2025-01-23 15:01:21 +0300

    Application Config

--- src/main/java/com/irfan/springframeworkindepth/Application.java
+++ src/main/java/com/irfan/springframeworkindepth/Application.java
@@ -1,6 +1,5 @@
 package com.irfan.springframeworkindepth;
 
-
 import com.irfan.springframeworkindepth.config.ApplicationConfig;
 import com.irfan.springframeworkindepth.service.OutputService;
 import org.springframework.context.ApplicationContext;
