97efc99b141cd198745971343b033e2f4c77a95c
Mary Wagura <waguramarie@gmail.com>
2025-02-03 08:50:39 +0300

    component scanning

--- src/main/java/com/irfan/springframeworkindepth/ApplicationConfig.java
+++ src/main/java/com/irfan/springframeworkindepth/ApplicationConfig.java
@@ -1,38 +1,9 @@
 package com.irfan.springframeworkindepth;
 
-import com.irfan.springframeworkindepth.service.GreetingService;
-import com.irfan.springframeworkindepth.service.OutputService;
-import com.irfan.springframeworkindepth.service.TimeService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+
 
 @PropertySource("classpath:application.properties")
 public class ApplicationConfig {
-    @Value("${app.greeting}")
-    private String greeting;
 
-    @Value("${app.name}")
-    private String name;
-
-    //go look for the profile
-    @Value("#{new Boolean(environment['spring.profiles.active']!='dev')}")
-    private boolean is24;
-
-    @Bean
-    public TimeService timeService() {
-        return new TimeService(is24);
-    }
-
-    @Bean
-    public OutputService outputService(
-            GreetingService greetingService,
-            TimeService timeService) {
-        return new OutputService(greetingService, timeService,name);
-    }
-
-    @Bean
-    public GreetingService greetingService() {
-        return new GreetingService(greeting);
-    }
 }
